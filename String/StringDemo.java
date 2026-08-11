b8a90cf3a178e285a01e205da54a9b0995ed372a
Divyansh Malvi <divymalvi2005@gmail.com>
2026-08-11 22:54:08 +0530

    String constant pool

--- String/StringDemo.java
+++ String/StringDemo.java
@@ -8,6 +8,17 @@ public class StringDemo {
 
 System.out.println("Hello " + name);
 System.out.println(name.charAt(3));
+// new object will be created for name , value of name is not changed or replaced with Divy.InsiDarr , new object has been created and old name object
+// will be eligible for garbage collection to free the memory
+name = "Divy." + name;
+// only 1 object will be created for S1 and S2
+String S1 = "divy";
+String S2 = "divy";
+
+System.out.println(name);
+System.out.println(S1.hashCode() + " | " + S2.hashCode());
+System.out.println(S1 == S2);
+
   }
   
 }

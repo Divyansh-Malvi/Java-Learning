d8acfad6d5780de0e1083dfd4063a790ff124e66
Divyansh Malvi <divymalvi2005@gmail.com>
2026-08-11 22:26:37 +0530

    Starting String

---
+++ String/StringDemo.java
@@ -0,0 +1,13 @@
+
+public class StringDemo {
+  public static void main(String[] args) {
+    
+    //declaration and initialisation
+
+    String name = "InsiDarr";
+
+System.out.println("Hello " + name);
+System.out.println(name.charAt(3));
+  }
+  
+}

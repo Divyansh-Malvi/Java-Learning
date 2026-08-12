be9819a216cc438c72c9e40667badbf811ab4745
Divyansh Malvi <divymalvi2005@gmail.com>
2026-08-12 23:58:11 +0530

    String continue

--- String/StringDemo.java
+++ String/StringDemo.java
@@ -3,6 +3,7 @@ public class StringDemo {
   public static void main(String[] args) {
     
     //declaration and initialisation
+    // String is actually a class
 
     String name = "InsiDarr";
 

d73b0a5d658744a346ca70c73763fb5326ccab22
Divyansh Malvi <divymalvi2005@gmail.com>
2026-08-16 23:23:01 +0530

    Learning  String Buffer

--- String/StringBufferDemo.java
+++ String/StringBufferDemo.java
@@ -1,6 +1,8 @@
 public class StringBufferDemo {
   public static void main(String[] args) {
     //Starting String buffer
+    StringBuffer sb = new StringBuffer("InsiDarr");
+    System.out.println(sb.capacity()); // 16bytes
   }
   
 }
